b6870e68dc0905724f83f7bb9af9eded5d33cc78
Denetor <gfc-com@mail.ru>
2018-05-08 15:30:06 +0300

    Create page "Hello world"

---
+++ frontend/controllers/HelloController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gfc-c
+ * Date: 08.05.2018
+ * Time: 15:20
+ */
+
+namespace frontend\controllers;
+
+
+class HelloController
+{
+
+}
---
+++ frontend/views/hello/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gfc-c
+ * Date: 08.05.2018
+ * Time: 15:19
+ */
