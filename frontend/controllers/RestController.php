9b1729d7f71d7601282b57dc857d56e643d9ea9d
Denetor <gfc-com@mail.ru>
2018-05-29 18:35:57 +0300

    Rest api GET/index

---
+++ frontend/controllers/RestController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace frontend\controllers;
+
+
+use common\models\Tasks;
+use yii\data\ActiveDataProvider;
+use yii\rest\ActiveController;
+
+
+class TaskAPIController extends ActiveController
+{
+
+   // public $modelClass = Tasks::class;
+
+    public function actionIndex(){
+
+        return new ActiveDataProvider([
+            'query' => Tasks::find()
+            ]
+        );
+
+    }
+
+}
